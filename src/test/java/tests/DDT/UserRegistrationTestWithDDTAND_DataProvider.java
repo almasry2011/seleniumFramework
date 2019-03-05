77ac595048ed46dc17e5eec4fafb089e9e7efd18
Amr Mohamed <a.mohamed@vp.local>
2019-03-05 12:02:43 +0200

    Selenium 002

--- src/test/java/tests/DDT/UserRegistrationTestWithDDTAND_DataProvider.java
+++ src/test/java/tests/DDT/UserRegistrationTestWithDDTAND_DataProvider.java
@@ -14,7 +14,7 @@ public class UserRegistrationTestWithDDTAND_DataProvider extends TestBase  {
 
 	String email="amr0111@gm.com";
 	String pass="amr123";
-    static String str="t04";
+    static String str="t05";
 
 	@DataProvider(name="testData")
 	public static Object [][] data(){
